c66b3c22527c720bf82a68b82613c55089034608
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 07:31:13 -0700

    - Created view model for main activity to act as the medium between the ui and the repository

---
+++ app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.nfragiskatos.fruitpicker_databinding
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Nicholas Fragiskatos on 4/18/2020.
+ */
+class MainViewModel : ViewModel() {
+}
