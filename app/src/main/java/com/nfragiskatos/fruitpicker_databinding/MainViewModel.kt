0c1801cabd04eb6df3bea0e82dcb02bbd119f5c7
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 07:33:13 -0700

    - Added variable for the UI to get the current random fruit name that actually just gets the value from the repository

--- app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.nfragiskatos.fruitpicker_databinding
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
 /**
  * Created by Nicholas Fragiskatos on 4/18/2020.
  */
 class MainViewModel : ViewModel() {
+    val currentRandomFruitName: LiveData<String>
+        get() = FakeRepository.currentRandomFruitName
 }
