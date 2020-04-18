089a806593b65c4f34d04e925438247f00bcebea
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 07:46:06 -0700

    - Got a reference to the MainViewModel (Singleton, so either creates or just gets it if it exists)

--- app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainActivity.kt
@@ -2,11 +2,15 @@ package com.nfragiskatos.fruitpicker_databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
     }
 }
