f0de296a01bc8e25ec6e5f7b0bb6923319a9082d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 07:20:09 -0700

    - Created fake repository object (not using a real backend)

---
+++ app/src/main/java/com/nfragiskatos/fruitpicker_databinding/FakeRepository.kt
@@ -0,0 +1,13 @@
+package com.nfragiskatos.fruitpicker_databinding
+
+/**
+ * Created by Nicholas Fragiskatos on 4/18/2020.
+ */
+object FakeRepository {
+    private val fruitNames: List<String> = listOf(
+        "Apple", "Banana", "Orange", "Kiwi", "Grapes", "Fig",
+        "Pear", "Strawberry", "Gooseberry", "Raspberry"
+    )
+
+
+}
