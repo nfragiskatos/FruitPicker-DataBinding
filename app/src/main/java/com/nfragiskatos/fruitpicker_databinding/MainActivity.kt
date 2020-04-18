e867e96249b00f815957c643912ce59d70f3b3a9
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 07:51:28 -0700

    - Added boilerplate code to initialize the binding between the live data and the resource we are inflating.

--- app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/fruitpicker_databinding/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.nfragiskatos.fruitpicker_databinding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.nfragiskatos.fruitpicker_databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        /*
+        ActivityMainBinding is an auto generated class. Whenever you use the <layout> + <data> tags in your activity xml
+        the class will be auto-generated
+        Boilerplate code....
+         */
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        ).apply {
+            this.lifecycleOwner = this@MainActivity
+            this.viewmodel = mainViewModel
+        }
 
     }
 }
