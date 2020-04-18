5ec4ae8b32d7a3dcf284899daafe2d463b6289b8
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 07:25:43 -0700

    - Added MutableLiveData + LiveData for the current random fruit name and initialized it

--- app/src/main/java/com/nfragiskatos/fruitpicker_databinding/FakeRepository.kt
+++ app/src/main/java/com/nfragiskatos/fruitpicker_databinding/FakeRepository.kt
@@ -1,5 +1,8 @@
 package com.nfragiskatos.fruitpicker_databinding
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 /**
  * Created by Nicholas Fragiskatos on 4/18/2020.
  */
@@ -9,5 +12,16 @@ object FakeRepository {
         "Pear", "Strawberry", "Gooseberry", "Raspberry"
     )
 
+    /*
+    We only want to expose data that can't be changed, so we have a public variable that is LiveData (Immutable)
+    and then the private variable (Mutable) which is the real data and we only want to change it within the
+    repository (information hiding, encapsulation)
+     */
+    private val _currentRandomFruitName = MutableLiveData<String>()
+    val currentRandomFruitName: LiveData<String>
+        get() = _currentRandomFruitName
 
+    init {
+        _currentRandomFruitName.value = fruitNames.first()
+    }
 }
